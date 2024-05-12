00837025331b9eeac74cc81e3d4ac69fadcd9115
Ebanx3 <esteban.dos.santos@gmail.com>
2024-05-12 19:26:00 -0300

    removed uuid and verify availability before to book

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.use(cors());
 
 const port =process.env.PORT || 8080;
 
-const turnos = [{fecha}];
+const turnos = [];
 
 server.get("/turnos", (req, res)=> {
     res.status(200).json({success:true, message: "Get turnos correctamente",data:turnos});
@@ -17,6 +17,10 @@ server.post("/turnos", (req, res) => {
     const { fecha, hora, nombre, telefono } = req.body;
     if(!fecha || !hora || !nombre || !telefono){
         return res.status(400).json({success: false, message: "Body debe tener: fecha, hora, nombre, telefono"});
+    }
+    const index = turnos.findIndex(turno => turno.fecha == fecha && turno.hora == hora);
+    if(index >= 0){
+        return res.status(400).json({success:false, message: "El horarios ya esta reservado"});
     }
     const nuevoTurno = {fecha, hora, nombre, telefono};
     turnos.push(nuevoTurno);
