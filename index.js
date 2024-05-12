b29575bb14acf456d853cdefdea425d2d2733af1
Julio Fleitas <122684703+juliofleitas@users.noreply.github.com>
2024-05-12 13:16:45 -0300

    up files
    up files

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {v4 as uuidv4} from "uuid"; 
+
+const server = express();
+server.use(express.json());
+
+const port =process.env.PORT || 8080;
+
+const turnos = [];
+
+server.get("/turnos", (req, res)=> {
+    res.status(200).json({success:true, message: "Get turnos correctamente",data:turnos});
+})
+
+server.post("/turnos", (req, res) => {
+    const { fecha, hora, nombre, telefono } = req.body;
+    if(!fecha || !hora || !nombre || !telefono){
+        return res.status(400).json({success: false, message: "Body debe tener: fecha, hora, nombre, telefono"});
+    }
+    const nuevoTurno = {fecha, hora, nombre, telefono, id: uuidv4()};
+    turnos.push(nuevoTurno);
+
+    res.status(201).json({success: true, message:"Agendado correctamente", data:turnos});
+})
+
+server.listen(port, ()=> console.log(`Server ready! -> http://localhost:${port}`));
